f6444487360973abebeaed47256ca41a10414569
yatochka-dev <philipchef13@gmail.com>
2025-10-28 13:28:08 +0200

    don't panic without .env file

--- core-svc/internal/util/config.go
+++ core-svc/internal/util/config.go
@@ -1,7 +1,9 @@
 package util
 
 import (
+	"log"
 	"os"
+	"strconv"
 
 	env "github.com/joho/godotenv"
 )
@@ -20,8 +22,8 @@ type Config struct {
 func LoadEnvironment() {
 	err := env.Load(".env")
 	if err != nil {
-		//log.Fatal("Unable to load the core enviroment file")
-		panic(".env" + err.Error())
+		log.Fatal("Unable to load the core enviroment file")
+		// panic(".env" + err.Error())
 	}
 
 }
@@ -37,14 +39,78 @@ func NewConfig() *Config {
 
 	appBaseURL := os.Getenv("APP_BASE_URL")
 	if appBaseURL == "" {
-		appBaseURL = "http://localhost:3000" // fallback for dev
+		appBaseURL = "http://localhost:8080" // fallback for dev
+	}
+
+	// AUTH SECRET
+	authSecret := os.Getenv("AUTH_SECRET")
+	if authSecret == "" {
+		authSecret = "hello"
+	}
+
+	// AUTH TOKEN LIFESPAN (hours) - parse to int64, default 24
+	authLifespan := int64(24)
+	if s := os.Getenv("AUTH_TOKEN_LIFESPAN_HOURS"); s != "" {
+		authLifespan, _ = strconv.ParseInt(s, 10, 64)
+	}
+
+	// ALLOWED ORIGINS - comma separated env var
+	defaultOrigins := []string{
+		"http://localhost:3000",
+		"http://127.0.0.1:3000",
+		"http://localhost:3000/",
+		"http://127.0.0.1:3000/",
+	}
+	allowedOrigins := []string{}
+	if s := os.Getenv("ALLOWED_ORIGINS"); s != "" {
+		// helper trim
+		trim := func(str string) string {
+			i, j := 0, len(str)-1
+			for i <= j {
+				c := str[i]
+				if c == ' ' || c == '\t' || c == '\n' || c == '\r' {
+					i++
+					continue
+				}
+				break
+			}
+			for j >= i {
+				c := str[j]
+				if c == ' ' || c == '\t' || c == '\n' || c == '\r' {
+					j--
+					continue
+				}
+				break
+			}
+			if i > j {
+				return ""
+			}
+			return str[i : j+1]
+		}
+
+		start := 0
+		for start < len(s) {
+			end := start
+			for end < len(s) && s[end] != ',' {
+				end++
+			}
+			token := trim(s[start:end])
+			if token != "" {
+				allowedOrigins = append(allowedOrigins, token)
+			}
+			start = end + 1
+		}
+	}
+
+	if len(allowedOrigins) == 0 {
+		allowedOrigins = defaultOrigins
 	}
 
 	return &Config{
 		DatabaseUrl:            os.Getenv("DATABASE_URL"),
-		AuthTokenLifespanHours: 24,
-		AuthSecret:             "hello",
-		AllowedOrigins:         []string{"http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3000/", "http://127.0.0.1:3000/"},
+		AuthTokenLifespanHours: authLifespan,
+		AuthSecret:             authSecret,
+		AllowedOrigins:         allowedOrigins,
 		CookieDomain:           cookieDomain,
 		CookieSecure:           cookieSecure,
 		ResendAPIKey:           os.Getenv("RESEND_API_KEY"),
